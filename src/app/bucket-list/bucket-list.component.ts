0037a4bf9ff053a2a9584ed889580d15a6efa3e2
JAN <janstefanlebar@gmail.com>
2024-05-10 16:38:52 +0200

    update

--- src/app/bucket-list/bucket-list.component.ts
+++ src/app/bucket-list/bucket-list.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { AlertModule } from 'ngx-bootstrap/alert';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-bucket-list',
   standalone: true,
-  imports: [AlertModule],
+  imports: [AlertModule, RouterLink, CommonModule],
   templateUrl: './bucket-list.component.html',
   styleUrl: './bucket-list.component.css'
 })
-export class BucketListComponent {
 
+
+export class BucketListComponent {
+  buckets: {id: number; name: string; }[] = [
+    { id: 1, name: 'Bucket 1' },
+    { id: 2, name: 'Bucket 2' },
+    { id: 3, name: 'Bucket 3' },
+    { id: 4, name: 'Bucket 4' },
+    { id: 5, name: 'Bucket 5' }
+  ];
 }
