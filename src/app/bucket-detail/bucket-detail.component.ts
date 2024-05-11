d1a4dcbeeda7ea8aaa9d16f25ec9b517144dc4f6
JAN <janstefanlebar@gmail.com>
2024-05-11 23:11:44 +0200

    file explorer

--- src/app/bucket-detail/bucket-detail.component.ts
+++ src/app/bucket-detail/bucket-detail.component.ts
@@ -15,4 +15,11 @@ export class BucketDetailComponent {
     this.bucketId = parseInt(this.route.snapshot.paramMap.get('id') || '0', 10);
     this.bucket = this.store.state().buckets.find(bucket => bucket.id === this.bucketId);
   }
+
+  onFileSelected(event: any) {
+    // Access the selected file here, you can use 'event.target.files' array
+    const file: File = event.target.files[0];
+    // Handle the file, you may want to upload it or perform other actions
+    console.log('Selected file:', file);
+  }
 }
