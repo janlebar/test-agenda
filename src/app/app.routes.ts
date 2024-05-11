6a607e6696990994c6ec617451c135e2e6d776b7
JAN <janstefanlebar@gmail.com>
2024-05-11 22:00:05 +0200

    second level

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,3 +7,23 @@ export const routes: Routes = [
     {path: 'cloudstorage', component: CloudStorageComponent},
 
 ];
+
+
+
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { BucketListComponent } from './bucket-list/bucket-list.component';
+// import { CloudStorageComponent } from './cloudstorage/cloudstorage.component';
+// import { BucketDetailComponent } from './bucket-detail.component'; // Import the component for bucket detail page
+
+// const routes: Routes = [
+//     { path: 'bucketlist', component: BucketListComponent },
+//     { path: 'cloudstorage', component: CloudStorageComponent },
+//     { path: 'bucket/:id', component: BucketDetailComponent } // Define route parameter for bucket ID
+// ];
+
+// @NgModule({
+//     imports: [RouterModule.forRoot(routes)],
+//     exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
