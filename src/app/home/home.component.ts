a321a98520a4ad405cdb44c55ba418ac19d993fb
JAN <janstefanlebar@gmail.com>
2024-05-12 21:58:34 +0200

    redesigning

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {BucketListComponent} from '../bucket-list/bucket-list.component';
+import {MainnavigationComponent} from '../mainnavigation/mainnavigation.component'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BucketListComponent],
+  imports: [BucketListComponent, MainnavigationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/mainnavigation/mainnavigation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-mainnavigation',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './mainnavigation.component.html',
+  styleUrl: './mainnavigation.component.css'
+})
+export class MainnavigationComponent {
+
+}
